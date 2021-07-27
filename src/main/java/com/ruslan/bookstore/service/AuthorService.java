12c12ad8a13fb66fbb415eba53f62e5a3e55a88c
Ruslan <r_nazar92@mail.ru>
2021-07-27 11:29:57 +0300

    add listAllAuthors method

--- src/main/java/com/ruslan/bookstore/service/AuthorService.java
+++ src/main/java/com/ruslan/bookstore/service/AuthorService.java
@@ -1,7 +1,22 @@
 package com.ruslan.bookstore.service;
 
+import com.ruslan.bookstore.Repository.AuthorRepository;
+import com.ruslan.bookstore.entity.Author;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AuthorService {
+    private AuthorRepository authorRepository;
+
+    @Autowired
+    public AuthorService(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    public List<Author> listAllAuthors() {
+        return authorRepository.findAll();
+    }
 }
