482104ace2aaee5870def6c6291df0e7e8d87190
Ruslan <r_nazar92@mail.ru>
2021-07-27 11:30:55 +0300

    add listAllGenres method

--- src/main/java/com/ruslan/bookstore/service/GenreService.java
+++ src/main/java/com/ruslan/bookstore/service/GenreService.java
@@ -1,7 +1,22 @@
 package com.ruslan.bookstore.service;
 
+import com.ruslan.bookstore.Repository.GenreRepository;
+import com.ruslan.bookstore.entity.Genre;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class GenreService {
+    private GenreRepository genreRepository;
+
+    @Autowired
+    public GenreService(GenreRepository genreRepository) {
+        this.genreRepository = genreRepository;
+    }
+
+    public List<Genre> listAllGenres(){
+        return genreRepository.findAll();
+    }
 }
