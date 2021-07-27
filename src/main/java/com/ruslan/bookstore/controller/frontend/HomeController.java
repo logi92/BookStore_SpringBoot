61ebf13c1ee509acda7368c6fcf15ac2c97493d2
Ruslan <r_nazar92@mail.ru>
2021-07-27 15:33:14 +0300

    add swagger

--- src/main/java/com/ruslan/bookstore/controller/frontend/HomeController.java
+++ src/main/java/com/ruslan/bookstore/controller/frontend/HomeController.java
@@ -2,9 +2,12 @@ package com.ruslan.bookstore.controller.frontend;
 
 import com.ruslan.bookstore.entity.Book;
 import com.ruslan.bookstore.service.BookServices;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -18,7 +21,7 @@ public class HomeController {
         this.bookServices = bookServices;
     }
 
-    @RequestMapping("")
+    @GetMapping("")
     public String listBooks(Model model) {
         List<Book> bookList = bookServices.listBooks();
         model.addAttribute("books", bookList);
